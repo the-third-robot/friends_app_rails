72cbb52b5de70cadd5b9dd6c2c76038fbf0610a8
Rushikesh Kumbhar <rushikeshkumbhar@golivio.com>
2023-05-03 18:36:15 +0530

    create friends schema, ui, add routes, update routes in nav-links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :friends
   # get 'home/index'
   # converts above root to the basic / root, i.e. initial root.
+  get 'home/about'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
